ca143fdc4e2aecd1486c77274da3339e71429a2d
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-04-13 09:36:24 +0200

    Control de COD de MAVEN - Documentacion

--- src/main/java/controlmaven/ControlMaven.java
+++ src/main/java/controlmaven/ControlMaven.java
@@ -4,8 +4,17 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Aplicacion Control Maven
+ * @author ldizbarros
+ */
 public class ControlMaven {
 
+    /**
+     * Clase principal de la aplicacion.
+     * En esta clase se llama a la clase metodos donde esta el codigo principal del programa.
+     * @param args 
+     */
     public static void main(String[] args) {
         try {
             Metodos.recogerMostrarDatos();
--- src/main/java/controlmaven/Metodos.java
+++ src/main/java/controlmaven/Metodos.java
@@ -3,11 +3,18 @@ package controlmaven;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-//import org.jsoup.nodes.Element;
-//import org.jsoup.select.Elements;
 
+/**
+ * Clase con los metodos del programa.
+ * @author ldizbarros
+ */
 public class Metodos {
     
+    /**
+     * Este metodo estatico recoge la informacion de la pagina web introducida
+     * y muestra por pantalla los datos recogidos.
+     * @throws IOException se lanza una excepcion en caso de un error con la conexion a la pagina web
+     */
     public static void recogerMostrarDatos() throws IOException{
         String url = "https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP";
         Document document = Jsoup.connect(url).get();
